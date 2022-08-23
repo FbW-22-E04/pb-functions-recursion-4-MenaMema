f19292a7bb9abed01e3a975059789b2b89fe8818
MenaMema <amenabarrera@gmail.com>
2022-08-23 14:23:50 +0200

    exercise

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // Write your code here:
-
-
+//function fibonacci(n) {
+//let sum = [];
+// if (n === 0) {
+//   return 0;
+// }
+//return n + fibonacci(n - 1);
+//}
+function fibonacci(n) {
+  var fibonacci = [];
+  fibonacci[0] = 0;
+  fibonacci[1] = 1;
+  for (var i = 2; i <= 8; i++) {
+    fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+  }
+  return fibonacci;
+}
 
 console.log(fibonacci(0)); // [0]
 console.log(fibonacci(1)); // [ 0, 1 ]
